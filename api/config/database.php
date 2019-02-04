6d0a0eeeb1dcfb2362a031b44214c57860918e4a
Erick Hernandez <erieduhergue11@gmail.com>
2019-02-04 14:24:36 -0700

    Database config api.

---
+++ api/config/database.php
@@ -0,0 +1,25 @@
+<?php
+  class Database {
+    // Database credentials
+    private $host = '';
+    private $db_name = '';
+    private $user_name = '';
+    private $password = '';
+    public $conn;
+
+    /**
+    * Get database connection.
+    **/
+    public function getConnection(){
+      $this->conn = null;
+
+      try {
+        $this->conn = new PDO('mysql:host='.$this->host.';dbname='.$this->db_name, $this->user_name, $this->password);
+        $this->conn->exec('set names utf8');
+      }catch(PDOException $exception){
+        echo 'connection error: '.$exception->getMessage();
+      }
+      return $this->conn;
+    }
+  }
+  }
